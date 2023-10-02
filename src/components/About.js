b654fa0648f7b503784ef3c27bb2d44fc7dd873f
Rachel Ehlers <rachel.ehlers1@gmail.com>
2023-10-02 13:20:35 -0400

    About page updates:

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,12 @@ function About() {
 
   return (
     <div className="row">
-      <div className="col-6">
+      <div className="col-7">
         &nbsp;
         <h1
           className="About"
           style={{
-            color: "#CBCB41",
+            color: "#cd2912",
             textAlign: "right",
             fontFamily: "faune",
             fontStyle: "bold",
@@ -33,12 +33,22 @@ function About() {
               fontStyle: "normal",
             }}
           >
-            When I'm not coding, I enjoy reading & spending time outdoors.
+            When I'm not coding, I enjoy hiking, traveling, and horseback riding.<br></br>{" "}
+          </special>
+          <special
+            style={{
+              color: "#FFFFFF",
+              textAlign: "right",
+              fontFamily: "faune",
+              fontStyle: "normal",
+            }}
+          >
+            My favorite genres are progressive house and trance. Check out what I'm listening to on Spotify: <br></br>{" "}
           </special>
         </h2>
       </div>
       <div className="col-5">
-        <img src="./images/Rachel-Ehlers.jpg"></img>
+        <img src="./images/Rachel-Cheng.jpg" className="propic" alt="Rachel Cheng"></img>
       </div>
       &nbsp;
     </div>
